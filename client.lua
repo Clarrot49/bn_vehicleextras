87533a7a966a3240d96b2f2fb5a760069cc804a3
Clarrot <168117558+Clarrot49@users.noreply.github.com>
2024-11-18 10:18:51 -0500

    Initial commit

---
+++ client.lua
@@ -0,0 +1,79 @@
+RegisterCommand("ex", function(source, args, rawCommand)
+    local player = PlayerPedId()
+    
+    if not IsPedInAnyVehicle(player, true) then
+        lib.notify({
+            title = 'Error:',
+            description = 'You must be in a vehicle to change extras.',
+            type = 'error'
+        })
+        return
+    end
+
+    if not args[1] then
+        lib.notify({
+            title = 'Error:',
+            description = 'You must specify an extra to change.',
+            type = 'error'
+        })
+        return
+    end
+
+    local vehicle = GetVehiclePedIsIn(player, false)
+    local extra = tonumber(args[1])
+
+    if not DoesExtraExist(vehicle, extra) then
+        lib.notify({
+            title = 'Error:',
+            description = 'This extra does not exist.',
+            type = 'error'
+        })
+        return
+    end
+    
+    local isExtraOn = IsVehicleExtraTurnedOn(vehicle, extra)
+  
+        if isExtraOn then
+            SetVehicleExtra(vehicle, extra, 1)
+        else
+            SetVehicleExtra(vehicle, extra, 0)
+        end
+end, false)
+
+
+RegisterCommand("livery", function(source, args, rawCommand)
+    local player = PlayerPedId()
+
+    if not IsPedInAnyVehicle(player, true) then
+        lib.notify({
+            title = 'Error:',
+            description = 'You must be in a vehicle to change liveries.',
+            type = 'error'
+        })
+        return
+    end
+
+    if not args[1] then
+        lib.notify({
+            title = 'Error:',
+            description = 'You must specify a livery number',
+            type = 'false'
+        })
+        return
+    end
+
+    local liveryNumber = tonumber(args[1])
+    local vehicle = GetVehiclePedIsIn(player, true)
+
+    if livery and livery >= 0 then
+        if livery < GetVehicleLiveryCount(vehicle) then
+            SetVehicleLivery(vehicle, liveryNumber)
+        else
+            lib.notify({
+                title = 'Error:',
+                description = 'Invalid livery number.',
+                type = 'error'
+            })
+        end
+    end
+end, false)
---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Clarrot49'
+description 'Simple script to toggle vehicle extras/livery with commands.'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
+
+shared_script "@ox_lib/init.lua"
